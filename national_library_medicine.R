1331ee4ec21c9f74ca00a57ec07f9a6b63563c32
jacksonmluckey <jacksonmluckey@gmail.com>
2020-05-06 09:27:56 -0700

    the list is somewhat correct

--- national_library_medicine.R
+++ national_library_medicine.R
@@ -1,13 +1,24 @@
 library(tidyverse)
-library(rvest)
+library(xml2)
 
 # https://collections.nlm.nih.gov/web_service.html
 
 url_base <- "https://wsearch.nlm.nih.gov/ws/query?db=digitalCollections"
 
+
+extract_nlm_entry <- function(nlm_entry) {
+  nlm_entry %>%
+    xml_children()
+}
+
+# takes a search term and queries the National Library of Medicine
 searchnlm <- function(term) {
   term <- paste0("&term=", term)
   url <- paste0(url_base, term)
-  print(url)
-  content <- read_xml(url)
+  tmp <- read_xml(url) %>%
+    xml_children()
+  content <- tmp[7] %>%
+    xml_children() %>%
+    xml_siblings %>%
+    map(extract_nlm_entry)
 }
