44e203b227b22e93f61629384a7fa9da1066ef7b
jacksonmluckey <jacksonmluckey@gmail.com>
2020-05-06 10:51:17 -0700

    starting to figure out xml2

--- national_library_medicine.R
+++ national_library_medicine.R
@@ -3,24 +3,12 @@ library(xml2)
 
 # https://collections.nlm.nih.gov/web_service.html
 
-url_base <- "https://wsearch.nlm.nih.gov/ws/query?db=digitalCollections"
-
-
-extract_nlm_entry <- function(nlm_entry) {
-  nlm_entry %>%
-    xml_children()
-}
-
 # takes a search term and queries the National Library of Medicine
 searchnlm <- function(term) {
   term <- paste0("&term=", term)
-  url <- paste0(url_base, term)
-  tmp <- read_xml(url) %>%
-    xml_children()
-  content <- tmp[7] %>%
-    xml_children() %>%
-    xml_siblings %>%
-    map(extract_nlm_entry)
+  url <- paste0("https://wsearch.nlm.nih.gov/ws/query?db=digitalCollections", term)
+  raw_xml <- read_xml(url)
+  documents <- raw_xml %>% xml_find_all(".//document")
 }
 
 test <- searchnlm("cholera")
