0a0ee7339588b4d1907a750d23edb3c78b85a906
jacksonmluckey <jacksonmluckey@gmail.com>
2020-05-10 13:16:05 -0700

    added email

--- R/national_library_medicine.R
+++ R/national_library_medicine.R
@@ -27,15 +27,17 @@ parse_document <- function(document) {
 }
 
 #' Takes a search term and queries the National Library of Medicine
-#' 
+#'
 #' @param term The search term to query
 #' @param retmax The number of observations you want to query. Defaults to 10.
-#' 
+#' @param email Your email address as a string. \n Optional--it allows the National Library of Medicine to contact you if there are problems with your queries.
+#'
 #' @example searchnlm("opioid")
+#' @export
 # term can be passed as a string with spaces, which will be treated as ANDs
 # or as a a character vector, where each element will be treated as ORs (in the future)
-searchnlm <- function(term, retmax = NA) {
-  
+searchnlm <- function(term, retmax = NA, email = NA) {
+
   # generate the URL to query
   # replace spaces in term with "+" as requested by
   term <- str_replace_all(term, " ", "+")
@@ -46,7 +48,11 @@ searchnlm <- function(term, retmax = NA) {
   if(!is.na(retmax)) {
     url <- paste0(url, "&retmax=", retmax)
   }
-  
+  # add email to url if it exists
+  if(!is.na(email)) {
+    url <- paste0(url, "&email=", email)
+  }
+
   # execute the query and parse the XML it returns
   raw_xml <- read_xml(url)
   documents <- raw_xml %>%
@@ -54,7 +60,7 @@ searchnlm <- function(term, retmax = NA) {
     xml_find_all(".//document")
   documents %>%
     map_dfr(parse_document)
-  
+
 }
 
 test <- searchnlm("cholera", retmax = 2000)
