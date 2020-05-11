ffcdc400117e6db2c099b48d9b0fd2a0aabcb3c0
jacksonmluckey <jacksonmluckey@gmail.com>
2020-05-11 11:29:19 -0700

    expanded documentation: added package docs, made NLM fields a nice looking list

---
+++ R/tidynlm.R
@@ -0,0 +1,9 @@
+#' tidynlm: An API Wrapper for the National Library of Medicine
+#'
+#' tidynlm provides access to the National Library of Medicine (NLM) database.
+#' Queries are executed using the \code{\link{searchnlm}} function. Individual
+#' documents can be downloaded using the URLs returned by searchnlm.
+#'
+#' @docType package
+#' @name tidynlm
+NULL
